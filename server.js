615f2da81241b76ceafd163f44b4960e5e036127
Alex Korjeski <akorjeski@gmail.com>
2020-03-30 14:18:26 -0400

    more heroku edits

--- server.js
+++ server.js
@@ -37,3 +37,5 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
+const port = process.env.PORT || 3000;
+app.listen(port);
