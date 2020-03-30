9c4431aad97ca657b806efa3116ae9641a264c5c
Alex Korjeski <akorjeski@gmail.com>
2020-03-30 15:00:43 -0400

    mongoose.connect was written twice

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 // If deployed on heroku, use the deployed database. Otherwise use the local workout database
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
-mongoose.connect(mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/akorjeski-fitness-tracker'));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/akorjeski-fitness-tracker');
 
 
 // routes
